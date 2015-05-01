edf7ad0dfa02c0252f3275d605f0007cb51fc003
SoulSven <wmoya@kippnyccp.org>
2015-05-01 09:04:57 -0400

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,7 +1,8 @@
 dice_tracker = {2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0 , 12:0}
 from random import randint
 
+for i in range(500):
+  combination = randint(1,6) + randint(1,6)
+  dice_tracker[combination] = dice_tracker[combination]+1
 
-randint(1,6) + randint(1,6)
-
-randint + randint
+print 
